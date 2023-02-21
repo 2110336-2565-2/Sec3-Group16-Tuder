9413e1b35bb49ac016a216832b8c771d6f20064d
Poon <6330137821@student.chula.ac.th>
2023-02-21 20:59:35 +0700

    Change button cursor
    Change button cursor to pointer

--- Frontend/src/components/FormStyle.js
+++ Frontend/src/components/FormStyle.js
@@ -70,6 +70,7 @@ const FormT = {
         border-radius: 6px;
         color:white;
         background-color: #EB7B42;
+        cursor: pointer;
         box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;  
         &:hover{
             background-color: rgb(240, 123, 36);
