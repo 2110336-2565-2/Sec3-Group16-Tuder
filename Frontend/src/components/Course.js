cb2aac49cdd0e8bebe003fafa7799551b7a9dc7e
DESKTOP-FLD7G2E\ball0 <ball04407@hotmail.com>
2023-02-22 18:23:46 +0700

    Add course list page

---
+++ Frontend/src/components/Course.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export default function Course(props){
+    return (
+        <>
+            <CardHeader>
+                <CardContent>
+                    {props.coursename}
+                </CardContent>
+                <CardContent>    
+                    {props.tutor}
+                </CardContent>
+                <CardContent>
+                    {props.content}
+                </CardContent>    
+            </CardHeader>
+        </>
+    )
+}
+
+
+
+const CardHeader = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+`
+
+const CardContent = styled.div`
+    display: flex;
+    font-size: 20px;
+`
--- Frontend/src/pages/Courses.js
+++ Frontend/src/pages/Courses.js
@@ -1,5 +1,37 @@
+import styled from 'styled-components';
+import courseItems from '../datas/course.json';
+import Course from '../components/Course';
+
 export default function Courses(){
+
+    
+
+
+
     return (
-        <h1>This is Courses Page</h1>
+        <>
+            <h1>Courses</h1>
+            <CourseListPage>
+                { courseItems.map(item => (  
+                    <CourseListcontent>
+                        <Course coursename={item.coursename} content={item.content} tutor={item.tutorname}/>       
+                    </CourseListcontent>
+                ))}
+            </CourseListPage>
+        </>
     )
 }
+
+const CourseListPage = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 20px;
+    justify-content: center;
+`
+
+const CourseListcontent = styled.div`
+    display: flex;
+    border: 1px solid black;
+    border-radius: 16px;
+    padding: 10px;
+`
