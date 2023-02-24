3874e0ae71f709ed1134600868497914991f1e43
Natt-s <Natthaphon.s2018@gmail.com>
2023-02-24 17:12:41 +0700

    Add schedule updating procedure to UpdateTutor repo

--- Backend/internal/schemas/schema.schedule.go
+++ Backend/internal/schemas/schema.schedule.go
@@ -1,12 +1,17 @@
 package schemas
 
-//type TimeSlot struct {
-//	Day  int `json:"day,omitempty"`
-//	Hour int `json:"hour,omitempty"`
-//}
-
-// a tuple specifying the occupied hour on a day of the week
-type Schedule struct {
+type TimeSlot struct {
 	Day  int `json:"day,omitempty"`
 	Hour int `json:"hour,omitempty"`
+}
+
+// a tuple specifying the occupied hour on a day of the week
+type Schedule []TimeSlot
+
+func (s *Schedule) AvailableTimeArrays() *[7][24]bool {
+	aT := [7][24]bool{}
+	for _, tS := range *s {
+		aT[tS.Day][tS.Hour] = true
+	}
+	return &aT
 }
