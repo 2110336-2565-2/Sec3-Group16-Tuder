f6eb6ad81d01179dca3ff1b58309fa5d164d6e04
ChayutAu <modunkung@gmail.com>
2023-02-25 03:49:21 +0700

    Auto stash before merge of "backend-AT/Te/tutor-edit-available-time" and "origin/backend-AT/Te/tutor-edit-available-time"

---
+++ Backend/internal/controllers/controller.student.go
@@ -0,0 +1,132 @@
+package controllers
+
+import (
+	"net/http"
+
+	schema "github.com/2110336-2565-2/Sec3-Group16-Tuder/internal/schemas"
+	service "github.com/2110336-2565-2/Sec3-Group16-Tuder/internal/services"
+	"github.com/labstack/echo/v4"
+)
+
+type controllerStudent struct {
+	studentService service.ServiceStudent
+}
+
+func NewControllerStudent(s service.ServiceStudent) *controllerStudent {
+	return &controllerStudent{studentService: s}
+}
+
+func (cS *controllerStudent) GetStudentByUsername(c echo.Context) (err error) {
+	uR := &schema.SchemaGetStudent{}
+	if err = c.Bind(uR); err != nil {
+		c.JSON(http.StatusBadRequest, schema.SchemaErrorResponse{
+			Success: false,
+			Message: "Invalid request",
+			Error:   err,
+		})
+		return
+	}
+
+	student, err := cS.studentService.GetStudentByUsername(uR)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, schema.SchemaErrorResponse{
+			Success: false,
+			Message: err.Error(),
+			Error:   err,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, schema.SchemaResponses{
+		Success: true,
+		Message: "Get student successfully",
+		Data:    student,
+	})
+	return nil
+}
+
+func (cS *controllerStudent) CreateStudent(c echo.Context) (err error) {
+	uR := &schema.SchemaCreateStudent{}
+	if err = c.Bind(uR); err != nil {
+		c.JSON(http.StatusBadRequest, schema.SchemaErrorResponse{
+			Success: false,
+			Message: "Invalid request",
+			Error:   err,
+		})
+		return
+	}
+
+	student, err := cS.studentService.CreateStudent(uR)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, schema.SchemaErrorResponse{
+			Success: false,
+			Message: err.Error(),
+			Error:   err,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, schema.SchemaResponses{
+		Success: true,
+		Message: "Create student successfully",
+		Data:    student,
+	})
+	return nil
+}
+
+func (cS *controllerStudent) UpdateStudent(c echo.Context) (err error) {
+	uR := &schema.SchemaUpdateStudent{}
+	if err = c.Bind(uR); err != nil {
+		c.JSON(http.StatusBadRequest, schema.SchemaErrorResponse{
+			Success: false,
+			Message: "Invalid request",
+			Error:   err,
+		})
+		return
+	}
+
+	student, err := cS.studentService.UpdateStudent(uR)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, schema.SchemaErrorResponse{
+			Success: false,
+			Message: err.Error(),
+			Error:   err,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, schema.SchemaResponses{
+		Success: true,
+		Message: "Update student successfully",
+		Data:    student,
+	})
+	return nil
+}
+
+func (cS *controllerStudent) DeleteStudent(c echo.Context) (err error) {
+	uR := &schema.SchemaDeleteStudent{}
+	if err = c.Bind(uR); err != nil {
+		c.JSON(http.StatusBadRequest, schema.SchemaErrorResponse{
+			Success: false,
+			Message: "Invalid request",
+			Error:   err,
+		})
+		return
+	}
+
+	err = cS.studentService.DeleteStudent(uR)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, schema.SchemaErrorResponse{
+			Success: false,
+			Message: err.Error(),
+			Error:   err,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, schema.SchemaResponses{
+		Success: true,
+		Message: "Delete student successfully",
+	})
+	return nil
+}
---
+++ Backend/internal/repositorys/repository.student.go
@@ -0,0 +1,148 @@
+package repositorys
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/2110336-2565-2/Sec3-Group16-Tuder/ent"
+	entStudent "github.com/2110336-2565-2/Sec3-Group16-Tuder/ent/student"
+	entUser "github.com/2110336-2565-2/Sec3-Group16-Tuder/ent/user"
+	schema "github.com/2110336-2565-2/Sec3-Group16-Tuder/internal/schemas"
+)
+
+type RepositoryStudent interface {
+	GetStudents() ([]*ent.Student, error)
+	GetStudentByUsername(sr *schema.SchemaGetStudent) (*ent.Student, error)
+	CreateStudent(sr *schema.SchemaCreateStudent) (*ent.Student, error)
+	UpdateStudent(sr *schema.SchemaUpdateStudent) (*ent.Student, error)
+	DeleteStudent(sr *schema.SchemaDeleteStudent) error
+}
+
+type repositoryStudent struct {
+	client *ent.Client
+	ctx    context.Context
+}
+
+func NewRepositoryStudent(c *ent.Client) RepositoryStudent {
+	return &repositoryStudent{client: c, ctx: context.Background()}
+}
+
+func (rS *repositoryStudent) GetStudents() ([]*ent.Student, error) {
+	students, err := rS.client.Student.Query().All(rS.ctx)
+	if err != nil {
+		return nil, err
+	}
+	return students, nil
+}
+
+func (rS *repositoryStudent) GetStudentByUsername(sr *schema.SchemaGetStudent) (*ent.Student, error) {
+	student, err := rS.client.Student.Query().Where(entStudent.HasUserWith(entUser.UsernameEQ(sr.Username))).WithUser().Only(rS.ctx)
+	if err != nil {
+		return nil, err
+	}
+	return student, nil
+}
+
+func (rS *repositoryStudent) CreateStudent(sr *schema.SchemaCreateStudent) (*ent.Student, error) {
+	// create a transaction
+	tx, err := rS.client.Tx(rS.ctx)
+	if err != nil {
+		return nil, fmt.Errorf("starting a transaction: %w", err)
+	}
+	// wrap the client with the transaction
+	txc := tx.Client()
+	// create a new user
+	newUser, err := txc.User.Create().
+		SetUsername(sr.Username).
+		SetPassword(sr.Password).
+		SetFirstName(sr.Firstname).
+		SetLastName(sr.Lastname).
+		SetPhone(sr.Phone).
+		SetAddress(sr.Address).
+		SetProfilePictureURL(sr.ProfilePictureURL).
+		Save(rS.ctx)
+
+	if err != nil {
+		if rerr := tx.Rollback(); rerr != nil {
+			return nil, fmt.Errorf("rolling back transaction: %v", rerr)
+		}
+	}
+	student, err := txc.Student.Create().
+		SetUser(newUser).
+		SetUserID(newUser.ID).
+		Save(rS.ctx)
+
+	if err != nil {
+		if rerr := tx.Rollback(); rerr != nil {
+			return nil, fmt.Errorf("rolling back transaction: %v", rerr)
+		}
+		return nil, fmt.Errorf("creating a new student: %w", err)
+	}
+
+	return student, tx.Commit()
+
+}
+
+func (rS *repositoryStudent) UpdateStudent(sr *schema.SchemaUpdateStudent) (*ent.Student, error) {
+	tx, err := rS.client.Tx(rS.ctx)
+	if err != nil {
+		return nil, fmt.Errorf("starting a transaction: %w", err)
+	}
+
+	txc := tx.Client()
+
+	user, err := txc.User.Query().
+		Where(entUser.Username(sr.Username)).
+		WithStudent().
+		Only(rS.ctx)
+
+	if err != nil {
+		if rerr := tx.Rollback(); rerr != nil {
+			return nil, fmt.Errorf("rolling back transaction: %v", rerr)
+		}
+		return nil, fmt.Errorf("querying a user: %w", err)
+	}
+
+	student := user.Edges.Student
+
+	user, err = txc.User.UpdateOne(user).
+		SetFirstName(sr.Firstname).
+		SetLastName(sr.Lastname).
+		SetPhone(sr.Phone).
+		SetAddress(sr.Address).
+		SetProfilePictureURL(sr.ProfilePictureURL).
+		Save(rS.ctx)
+
+	if err != nil {
+		if rerr := tx.Rollback(); rerr != nil {
+			return nil, fmt.Errorf("rolling back transaction: %v", rerr)
+		}
+		return nil, fmt.Errorf("updating a user: %w", err)
+	}
+
+	student.Edges.User = user
+	return student, tx.Commit()
+}
+
+func (rS *repositoryStudent) DeleteStudent(sr *schema.SchemaDeleteStudent) error {
+	tx, err := rS.client.Tx(rS.ctx)
+	if err != nil {
+		return fmt.Errorf("starting a transaction: %w", err)
+	}
+
+	txc := tx.Client()
+
+	err = txc.Student.
+		DeleteOneID(sr.ID).
+		Exec(rS.ctx)
+
+	if err != nil {
+		if rerr := tx.Rollback(); rerr != nil {
+			return fmt.Errorf("starting a transaction %w", rerr)
+		}
+		return err
+	}
+
+	return nil
+
+}
