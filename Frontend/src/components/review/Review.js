c42426c65443a8d8dbd62d71d7fa09a72e6025e3
Poon <6330137821@student.chula.ac.th>
2023-03-31 12:58:29 +0700

    Add review_time_at to Review

--- Frontend/src/components/review/Review.js
+++ Frontend/src/components/review/Review.js
@@ -6,12 +6,20 @@ export default function Review({ review }) {
   const [score, setScore] = useState(parseInt(review.score));
   return (
     <Container>
-      <Rating
-        initialValue={score}
-        readonly={true}
-        size="20"
-        fillColor="#FF5D02"
-      />
+      {review.course_title? <h3>{review.course_title}</h3> : null}
+      <RowWrapper>
+        <Rating
+          initialValue={score}
+          readonly={true}
+          size="20"
+          fillColor="#FF5D02"
+        />
+        <ReviewTime>
+          {review.review_time_at
+            ? review.review_time_at.split("T")[0]
+            : review.review_time_at}
+        </ReviewTime>
+      </RowWrapper>
       <ReviewMsg>{review.review_msg}</ReviewMsg>
     </Container>
   );
@@ -30,3 +38,12 @@ const Container = styled.div`
 const ReviewMsg = styled.p`
   margin-top: 20px;
 `;
+
+const RowWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+const ReviewTime = styled.p``;
--- Frontend/src/datas/DummyReview.js
+++ Frontend/src/datas/DummyReview.js
@@ -13,18 +13,56 @@ export const courseReviews = {
         {
             "score": "5",
             "review_msg": "This course is very good. I like it very much.",
+            "review_time_at":"2021-02-18T21:54:42.123Z"
         },
         {
             "score": "4",
             "review_msg": "This course is very much. I like it very good.",
+            "review_time_at":"2021-02-18T21:54:42.123Z"
         },
         {
             "score": "5",
             "review_msg": "great course, only teaching what's needed, doesn't push you a lot in the coding assignments, as much as it requires you much more work to understand the codes and the science behind it.",
+            "review_time_at":"2021-03-18T21:54:42.123Z"
         },
         {
             "score": "4",
             "review_msg": "Excellent course. The videos were a pleasure to watch, the assignments were clear and allowed you to go as shallow or as in depth as you desired, and the mentors were very helpful.",
+            "review_time_at":"2021-04-18T21:54:42.123Z"
         }
     ]
 }
+
+export const tutorReviews = {
+    "tutor_firstname" : "Andrew",
+    "tutor_lastname" : "Ng",
+    "total_score": "4.5", // 2 decimal places
+    "total_review": "4", // if there is no review, total_review = 0
+    "reviews": [
+        {
+            "course_title": "Deep Learning Specialization",
+            "score": "5",
+            "review_msg": "I don't worry anymore.",
+            "review_time_at":"2021-02-18T21:54:42.123Z"
+        },
+        {
+            "course_title": "Deep Learning Specialization",
+            "score": "4",
+            "review_msg": "I like it very good.",
+            "review_time_at":"2021-02-18T21:54:42.123Z"
+        },
+        {
+            "course_title": "Machine Learning Specialization",
+            "score": "5",
+            "review_msg": "great course, only teaching what's needed, doesn't push you a lot in the coding assignments, as much as it requires you much more work to understand the codes and the science behind it.",
+            "review_time_at":"2021-02-18T21:54:42.123Z"
+        },
+        {
+            "course_title": "Machine Learning Specialization",
+            "score": "4",
+            "review_msg": "Excellent course. The videos were a pleasure to watch, the assignments were clear and allowed you to go as shallow or as in depth as you desired, and the mentors were very helpful.",
+            "review_time_at":"2021-02-18T21:54:42.123Z"
+        }
+    ]
+}
+
