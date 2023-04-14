adec1b23b13811a56c8b2e1193f46737859755e8
Natchapon Chitturongaporn <88874631+moocandygumlol@users.noreply.github.com>
2023-04-14 16:13:24 +0700

    fix: Homepage

--- Frontend/src/pages/Home.js
+++ Frontend/src/pages/Home.js
@@ -1,35 +1,101 @@
 import React from 'react'
 import styled from 'styled-components'
-import Footer from "../components/global/Footer.js";
-
+import { Link } from 'react-router-dom';
 export default function Home(){
     return (
-        <Container>
-            <FlexHome>
-                <FlexHeader>Learn desired course</FlexHeader>
-                <FlexHeader>From your desired</FlexHeader>
-                <FlexHeader>tutor</FlexHeader>
-                <FlexContent>
-                Are you looking to enhance your knowledge, acquire new skills, or boost your career prospects? 
-                Look no further! Our study course is designed to provide you with the knowledge, resources, and support you need to achieve your goals.
-                </FlexContent>
-                <FlexButtonRow>
-                    <FlexButtonContent>
-                        <StartButton>Start Learning</StartButton>
-                    </FlexButtonContent>
-                    <FlexButtonContent>
-                        <StartButton></StartButton>
-                    </FlexButtonContent>
-                </FlexButtonRow>
-            </FlexHome>
-        </Container>
+        <>
+            <HomeContainer>
+                <HomeHeader>
+                    <HomeTitle>Welcome to Tudor</HomeTitle>
+                    <HomeSubtitle>The ultimate platform for online learning</HomeSubtitle>
+                </HomeHeader>
+                <Container>
+                    <HomeDescription>
+                    <HomeFeature>
+                        <HomeFeatureTitle>Learn at Your Own Place</HomeFeatureTitle>
+                        <HomeFeatureDescription>Tuder provides a flexible learning experience, allowing you to learn at your own place, anywhere.</HomeFeatureDescription>
+                    </HomeFeature>
+                    <HomeFeature>
+                        <HomeFeatureTitle>Access to High-Quality Content</HomeFeatureTitle>
+                        <HomeFeatureDescription>Our platform offers a vast library of courses, carefully curated by experts in various fields.</HomeFeatureDescription>
+                    </HomeFeature>
+                    <HomeFeature>
+                        <HomeFeatureTitle>Interactive Learning Tools</HomeFeatureTitle>
+                        <HomeFeatureDescription>Engage in interactive learning with discussions, designed to enhance your understanding and easy to use.</HomeFeatureDescription>
+                    </HomeFeature>
+                    </HomeDescription>
+                </Container>
+            </HomeContainer>
+        </>
     )
 }
 
 const Container = styled.div`
-    margin-top: 4%;
-    margin-left: 7%;
-    margin-right: 7%;
+    margin-top: 7%;
+    margin-left: 4%;
+    margin-right: 4%;
+    margin-bottom: 4%;
+    border-radius: 10px;
+    padding: 3%;
+`
+
+const HomeContainer = styled.div`
+    max-width: 1200px;
+    margin: 0 auto;
+`
+
+const HomeHeader = styled.div`
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 60px;
+    height: 500px;
+    background-image: linear-gradient(rgba(0, 0, 0, 0.60), rgba(0, 0, 0, 0.60)), url("/images/backgroundtudor.jpg");
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-position: bottom;
+    background-size: cover;
+    
+`
+
+const HomeTitle = styled.div`
+    font-size: 45px;
+    font-weight: bold;
+    margin: 0;
+    color: #FF702F;
+`
+const HomeSubtitle = styled.div`
+    font-size: 18px;
+    color: #EEEEEE;
+    margin-top: 10px;
+`
+
+const HomeDescription = styled.div`
+    display: flex;
+    justify-content: center;
+    gap: 40px;
+`
+
+const HomeFeature = styled.div`
+    flex-basis: 33.33%;
+    text-align: center;
+`
+
+const HomeFeatureTitle = styled.h2`
+    font-size: 24px;
+    font-weight: bold;
+    margin-top: 20px;
+`
+
+const HomeFeatureDescription = styled.div`
+    font-size: 16px;
+    color: #666;
+    margin-top: 10px;
+`
+const GapLink = styled.div`
+    padding: 3px;
 `
 const FlexHome = styled.div`
     display: flex;
