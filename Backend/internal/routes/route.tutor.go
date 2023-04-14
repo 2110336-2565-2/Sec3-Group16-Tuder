95e110b2b842f4005ccaf231b30c3b4f7a548a2f
Bxntn <nuttanan118123@gmail.com>
2023-04-14 17:26:41 +0700

    feat: Add middleware for tutor to claim jwt

--- Backend/internal/routes/route.tutor.go
+++ Backend/internal/routes/route.tutor.go
@@ -1,10 +1,12 @@
 package routes
 
 import (
+	"os"
+
 	"github.com/2110336-2565-2/Sec3-Group16-Tuder/ent"
 	controller "github.com/2110336-2565-2/Sec3-Group16-Tuder/internal/controllers"
 
-	// "github.com/2110336-2565-2/Sec3-Group16-Tuder/internal/middlewares"
+	"github.com/2110336-2565-2/Sec3-Group16-Tuder/internal/middlewares"
 	repository "github.com/2110336-2565-2/Sec3-Group16-Tuder/internal/repositorys"
 	service "github.com/2110336-2565-2/Sec3-Group16-Tuder/internal/services"
 	echo "github.com/labstack/echo/v4"
@@ -16,7 +18,7 @@ func InitTutorRoutes(client *ent.Client, e *echo.Group) {
 	repoTutor := repository.NewRepositoryTutor(client)
 	serviceTutor := service.NewServiceTutor(repoTutor)
 	controllerTutor := controller.NewControllerTutor(serviceTutor)
-	// authMiddleware := middlewares.NewAuthMiddleware(os.Getenv("JWT_SECRET"))
+	authMiddleware := middlewares.NewAuthMiddleware(os.Getenv("JWT_SECRET"))
 
 	e.GET("/tutor/:username", controllerTutor.GetTutorByUsername)
 	e.GET("/tutorID/:id", controllerTutor.GetTutorByID)
@@ -28,4 +30,5 @@ func InitTutorRoutes(client *ent.Client, e *echo.Group) {
 	e.GET("/tutor/schedule", controllerTutor.GetTutorSchedule)
 	e.GET("/tutor/schedule/:courseId", controllerTutor.GetTutorScheduleByCourseId)
 	e.GET("/tutor/:username/reviews", controllerTutor.GetTutorReviews)
+	e.GET("/tutor/courses", controllerTutor.GetTutorCourses, authMiddleware.JWT())
 }
