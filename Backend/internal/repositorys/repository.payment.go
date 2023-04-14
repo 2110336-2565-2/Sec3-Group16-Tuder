7bdaf08415817b4d9bee522efd80d3454dfd98be
Nattawat Kwansakul <champ_2543@hotmail.com>
2023-04-14 18:01:55 +0700

    feat: add create source in get qr code

--- Backend/internal/repositorys/repository.payment.go
+++ Backend/internal/repositorys/repository.payment.go
@@ -1,23 +1,16 @@
 package repositorys
 
 import (
-	"bytes"
 	"context"
-	"fmt"
-	"image/png"
 	"log"
-	"math/rand"
 	"os"
-	"time"
 
 	"github.com/2110336-2565-2/Sec3-Group16-Tuder/ent/course"
 	schema "github.com/2110336-2565-2/Sec3-Group16-Tuder/internal/schemas"
-	utils "github.com/2110336-2565-2/Sec3-Group16-Tuder/internal/utils"
 
 	"github.com/2110336-2565-2/Sec3-Group16-Tuder/ent"
 	"github.com/omise/omise-go"
 	"github.com/omise/omise-go/operations"
-	"github.com/skip2/go-qrcode"
 )
 
 type RepositoryPayment interface {
@@ -56,68 +49,61 @@ func (r *repositoryPayment) GetQRCode(sr *schema.SchemaGetQRCode) (string, error
 		return "", err
 	}
 
-	fmt.Println("tutor: ", course.Edges.Tutor)
+	// Create a new source
+	source, createSource := &omise.Source{}, &operations.CreateSource{
+		Amount:   amount,
+		Currency: "THB",
+		Type:     "promptpay",
+	}
+
+	if e := client.Do(source, createSource); e != nil {
+		log.Fatal(e)
+	}
 
 	// Create a new charge
 	charge, createCharge := &omise.Charge{}, &operations.CreateCharge{
 		Amount:   amount,
 		Currency: "thb",
 		Customer: *course.Edges.Tutor.OmiseCustomerID,
+		Source:   source.ID,
 	}
-
-	// // Set the expiration time to 5 minutes from now
-	// charge.Metadata["expiration_time"] = time.Now().Add(time.Minute * 5).Format(time.RFC3339)
 
 	if e := client.Do(charge, createCharge); e != nil {
 		log.Fatal(e)
 	}
 
-	log.Printf("charge: %s  amount: %s %d\n", charge.ID, charge.Currency, charge.Amount)
+	qrCodePictureURL := charge.Source.ScannableCode.Image.DownloadURI
 
-	// Create a new QR code
-	qrCode, err := qrcode.New(charge.Source.ScannableCode.Image.Object, qrcode.Medium)
+	// // Create a new QR code
+	// qrCode, err := qrcode.New(charge.Source.ScannableCode.Image.Object, qrcode.Medium)
 
-	if err != nil {
-		return "", err
-	}
+	// if err != nil {
+	// 	return "", err
+	// }
 
-	fmt.Println("QR Code: ", qrCode)
+	// // Resize the image to 400x400 pixels
+	// qrImage := qrCode.Image(400)
 
-	// Resize the image to 400x400 pixels
-	qrImage := qrCode.Image(400)
-
-	fmt.Println("QR Image: ", qrImage)
-
-	// Convert the image to Byte
-	var buf bytes.Buffer
-	if err := png.Encode(&buf, qrImage); err != nil {
-		return "", err
-	}
-
-	fmt.Println("buf.Bytes(): ", buf.Bytes())
-
-	qrCodePictureURL, _ := utils.GenerateProfilePictureURL(buf.Bytes(), r.RandomQRCodeKey(), "QRCode")
-
+	// // Convert the image to Byte
 	// var buf bytes.Buffer
 	// if err := png.Encode(&buf, qrImage); err != nil {
 	// 	return "", err
 	// }
 
-	// Encode the PNG image to base64 string
-	// qrCodeBase64 := base64.StdEncoding.EncodeToString(buf.Bytes())
-
+	// qrCodePictureURL, _ := utils.GenerateProfilePictureURL(buf.Bytes(), r.RandomQRCodeKey(), "QRCode")
 	return qrCodePictureURL, nil
 }
 
-func (r *repositoryPayment) RandomQRCodeKey() string {
-	rand.Seed(time.Now().UnixNano())
+//For traditional QR code URL
+// func (r *repositoryPayment) RandomQRCodeKey() string {
+// 	rand.Seed(time.Now().UnixNano())
 
-	const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+// 	const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-	b := make([]byte, 12)
-	for i := range b {
-		b[i] = letterBytes[rand.Intn(len(letterBytes))]
-	}
+// 	b := make([]byte, 12)
+// 	for i := range b {
+// 		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+// 	}
 
-	return string(b)
-}
+// 	return string(b)
+// }
