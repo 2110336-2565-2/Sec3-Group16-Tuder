d75d9b0cd729766cae81871d12159f5c9f7eac8a
Nattawat Kwansakul <champ_2543@hotmail.com>
2023-04-15 21:57:03 +0700

    feat: add webhook charge handler for changing status charge

--- Backend/internal/repositorys/repository.payment.go
+++ Backend/internal/repositorys/repository.payment.go
@@ -2,11 +2,14 @@ package repositorys
 
 import (
 	"context"
+	"errors"
 	"log"
 	"os"
 
 	"github.com/2110336-2565-2/Sec3-Group16-Tuder/ent/course"
+	entPayment "github.com/2110336-2565-2/Sec3-Group16-Tuder/ent/payment"
 	"github.com/2110336-2565-2/Sec3-Group16-Tuder/ent/tutor"
+	"github.com/2110336-2565-2/Sec3-Group16-Tuder/internal/schemas"
 	schema "github.com/2110336-2565-2/Sec3-Group16-Tuder/internal/schemas"
 
 	"github.com/2110336-2565-2/Sec3-Group16-Tuder/ent"
@@ -15,8 +18,9 @@ import (
 )
 
 type RepositoryPayment interface {
-	GetQRCodeForCoursePayment(sr *schema.SchemaGetQRCodeForCoursePayment) (string, error)
-	GetQRCodeForTuitionFree(sr *schema.SchemaGetQRCodeForTuitionFree) (string, error)
+	GetQRCodeForCoursePayment(sr *schema.SchemaGetQRCodeForCoursePayment) (*schemas.SchemaQRCode, error)
+	GetQRCodeForTuitionFree(sr *schema.SchemaGetQRCodeForTuitionFree) (*schemas.SchemaQRCode, error)
+	WebhookChargeHandler(sr *schema.SchemaChargeWebhook) (*ent.Payment, error)
 }
 
 type repositoryPayment struct {
@@ -28,7 +32,7 @@ func NewRepositoryPayment(c *ent.Client) *repositoryPayment {
 	return &repositoryPayment{client: c, ctx: context.Background()}
 }
 
-func (r *repositoryPayment) GetQRCodeForCoursePayment(sr *schema.SchemaGetQRCodeForCoursePayment) (string, error) {
+func (r *repositoryPayment) GetQRCodeForCoursePayment(sr *schema.SchemaGetQRCodeForCoursePayment) (*schemas.SchemaQRCode, error) {
 	omisePublicKey := os.Getenv("OMISE_PUBLIC_KEY")
 	omiseSecretKey := os.Getenv("OMISE_SECRET_KEY")
 
@@ -44,11 +48,13 @@ func (r *repositoryPayment) GetQRCodeForCoursePayment(sr *schema.SchemaGetQRCode
 	course, err := r.client.Course.
 		Query().
 		Where(course.IDEQ(sr.Course_id)).
-		WithTutor().
+		WithTutor(func(tq *ent.TutorQuery) {
+			tq.WithUser()
+		}).
 		Only(r.ctx)
 
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	// Create a new source
@@ -75,10 +81,30 @@ func (r *repositoryPayment) GetQRCodeForCoursePayment(sr *schema.SchemaGetQRCode
 	}
 
 	qrCodePictureURL := charge.Source.ScannableCode.Image.DownloadURI
-	return qrCodePictureURL, nil
+
+	// Create Payment record
+	payment, err := r.client.Payment.
+		Create().
+		SetAmount(sr.Amount).
+		SetUserID(course.Edges.Tutor.Edges.User.ID).
+		SetPaymentStatus("processing").
+		SetQrPictureURL(qrCodePictureURL).
+		SetCurrency("THB").
+		SetChargeID(charge.ID).
+		Save(r.ctx)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &schema.SchemaQRCode{
+		Payment:   payment,
+		QrCodeUrl: qrCodePictureURL,
+		Charge_id: charge.ID,
+	}, nil
 }
 
-func (r *repositoryPayment) GetQRCodeForTuitionFree(sr *schema.SchemaGetQRCodeForTuitionFree) (string, error) {
+func (r *repositoryPayment) GetQRCodeForTuitionFree(sr *schema.SchemaGetQRCodeForTuitionFree) (*schemas.SchemaQRCode, error) {
 	omisePublicKey := os.Getenv("OMISE_PUBLIC_KEY")
 	omiseSecretKey := os.Getenv("OMISE_SECRET_KEY")
 
@@ -94,10 +120,11 @@ func (r *repositoryPayment) GetQRCodeForTuitionFree(sr *schema.SchemaGetQRCodeFo
 	tutor, err := r.client.Tutor.
 		Query().
 		Where(tutor.IDEQ(sr.Tutor_id)).
+		WithUser().
 		Only(r.ctx)
 
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	// Create a new source
@@ -124,5 +151,99 @@ func (r *repositoryPayment) GetQRCodeForTuitionFree(sr *schema.SchemaGetQRCodeFo
 	}
 
 	qrCodePictureURL := charge.Source.ScannableCode.Image.DownloadURI
-	return qrCodePictureURL, nil
+
+	// Create Payment record
+	payment, err := r.client.Payment.
+		Create().
+		SetAmount(sr.Amount).
+		SetUserID(tutor.Edges.User.ID).
+		SetPaymentStatus("processing").
+		SetQrPictureURL(qrCodePictureURL).
+		SetCurrency("THB").
+		SetChargeID(charge.ID).
+		SetAppointmentID(sr.Appointment_id).
+		Save(r.ctx)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &schema.SchemaQRCode{
+		Payment:   payment,
+		QrCodeUrl: qrCodePictureURL,
+		Charge_id: charge.ID,
+	}, nil
+}
+
+func (r *repositoryPayment) WebhookChargeHandler(sr *schema.SchemaChargeWebhook) (*ent.Payment, error) {
+	// Check if charge is pending
+	if sr.Data.Status == "pending" {
+		// Check if payment exists
+		payment, err := r.client.Payment.
+			Query().
+			Where(entPayment.ChargeID(sr.Data.ID)).
+			Only(r.ctx)
+
+		if err != nil {
+			return nil, err
+		}
+
+		// Update payment status to pending
+		_, err = r.client.Payment.
+			UpdateOne(payment).
+			SetPaymentStatus("pending").
+			Save(r.ctx)
+
+		if err != nil {
+			return nil, err
+		}
+		return payment, nil
+
+	} else if sr.Data.Status == "successful" {
+		// Check if payment exists
+		payment, err := r.client.Payment.
+			Query().
+			Where(entPayment.ChargeID(sr.Data.ID)).
+			Only(r.ctx)
+
+		if err != nil {
+			return nil, err
+		}
+
+		// Update payment status to successful
+		_, err = r.client.Payment.
+			UpdateOne(payment).
+			SetPaymentStatus("successful").
+			Save(r.ctx)
+
+		if err != nil {
+			return nil, err
+		}
+		return payment, nil
+
+	} else if sr.Data.Status == "failed" {
+		// Check if payment exists
+		payment, err := r.client.Payment.
+			Query().
+			Where(entPayment.ChargeID(sr.Data.ID)).
+			Only(r.ctx)
+
+		if err != nil {
+			return nil, err
+		}
+
+		// Update payment status to successful
+		_, err = r.client.Payment.
+			UpdateOne(payment).
+			SetPaymentStatus("failed").
+			Save(r.ctx)
+
+		if err != nil {
+			return nil, err
+		}
+		return payment, nil
+
+	} else {
+		return nil, errors.New("payment status is not valid")
+	}
 }
--- Backend/internal/schemas/schema.payment.go
+++ Backend/internal/schemas/schema.payment.go
@@ -3,11 +3,14 @@ package schemas
 import (
 	"time"
 
+	"github.com/2110336-2565-2/Sec3-Group16-Tuder/ent"
 	"github.com/google/uuid"
 )
 
 type SchemaQRCode struct {
-	QrCodeUrl string `json:"qr_code_url"`
+	QrCodeUrl string       `json:"qr_code_url"`
+	Charge_id string       `json:"charge_id"`
+	Payment   *ent.Payment `json:"payment"`
 }
 
 type SchemaGetQRCodeForCoursePayment struct {
@@ -16,8 +19,9 @@ type SchemaGetQRCodeForCoursePayment struct {
 }
 
 type SchemaGetQRCodeForTuitionFree struct {
-	Tutor_id uuid.UUID `json:"tutor_id"`
-	Amount   int       `json:"amount" validate:"required"`
+	Appointment_id uuid.UUID `json:"appointment_id"`
+	Tutor_id       uuid.UUID `json:"tutor_id"`
+	Amount         int       `json:"amount" validate:"required"`
 }
 
 type Card struct {
@@ -35,4 +39,134 @@ type Card struct {
 	Fingerprint       string `json:"fingerprint"`
 	Name              string `json:"name"`
 	SecurityCodeCheck bool   `json:"security_code_check"`
+}
+
+type SchemaChargeWebhook struct {
+	Object            string        `json:"object"`
+	ID                string        `json:"id"`
+	Livemode          bool          `json:"livemode"`
+	Location          string        `json:"location"`
+	WebhookDeliveries []interface{} `json:"webhook_deliveries"`
+	Data              struct {
+		Object            string `json:"object"`
+		ID                string `json:"id"`
+		Location          string `json:"location"`
+		Amount            int    `json:"amount"`
+		AuthorizationType string `json:"authorization_type"`
+		AuthorizedAmount  int    `json:"authorized_amount"`
+		CapturedAmount    int    `json:"captured_amount"`
+		Net               int    `json:"net"`
+		Fee               int    `json:"fee"`
+		FeeVat            int    `json:"fee_vat"`
+		Interest          int    `json:"interest"`
+		InterestVat       int    `json:"interest_vat"`
+		FundingAmount     int    `json:"funding_amount"`
+		RefundedAmount    int    `json:"refunded_amount"`
+		TransactionFees   struct {
+			FeeFlat string `json:"fee_flat"`
+			FeeRate string `json:"fee_rate"`
+			VatRate string `json:"vat_rate"`
+		} `json:"transaction_fees"`
+		PlatformFee struct {
+			Fixed      string `json:"fixed"`
+			Amount     string `json:"amount"`
+			Percentage string `json:"percentage"`
+		} `json:"platform_fee"`
+		Currency        string `json:"currency"`
+		FundingCurrency string `json:"funding_currency"`
+		IP              string `json:"ip"`
+		Refunds         struct {
+			Object   string        `json:"object"`
+			Data     []interface{} `json:"data"`
+			Limit    int           `json:"limit"`
+			Offset   int           `json:"offset"`
+			Total    int           `json:"total"`
+			Location string        `json:"location"`
+			Order    string        `json:"order"`
+			From     time.Time     `json:"from"`
+			To       time.Time     `json:"to"`
+		} `json:"refunds"`
+		Link        string `json:"link"`
+		Description string `json:"description"`
+		Metadata    struct {
+		} `json:"metadata"`
+		Card   string `json:"card"`
+		Source struct {
+			Object          string    `json:"object"`
+			ID              string    `json:"id"`
+			Livemode        bool      `json:"livemode"`
+			Location        string    `json:"location"`
+			Amount          int       `json:"amount"`
+			Barcode         string    `json:"barcode"`
+			Bank            string    `json:"bank"`
+			CreatedAt       time.Time `json:"created_at"`
+			Currency        string    `json:"currency"`
+			Email           string    `json:"email"`
+			Flow            string    `json:"flow"`
+			InstallmentTerm string    `json:"installment_term"`
+			AbsorptionType  string    `json:"absorption_type"`
+			Name            string    `json:"name"`
+			MobileNumber    string    `json:"mobile_number"`
+			PhoneNumber     string    `json:"phone_number"`
+			PlatformType    string    `json:"platform_type"`
+			ScannableCode   struct {
+				Object string `json:"object"`
+				Type   string `json:"type"`
+				Image  struct {
+					Object      string    `json:"object"`
+					Livemode    bool      `json:"livemode"`
+					ID          string    `json:"id"`
+					Deleted     bool      `json:"deleted"`
+					Filename    string    `json:"filename"`
+					Location    string    `json:"location"`
+					Kind        string    `json:"kind"`
+					DownloadURI string    `json:"download_uri"`
+					CreatedAt   time.Time `json:"created_at"`
+				} `json:"image"`
+			} `json:"scannable_code"`
+			Billing                  string        `json:"billing"`
+			Shipping                 string        `json:"shipping"`
+			Items                    []interface{} `json:"items"`
+			References               string        `json:"references"`
+			StoreID                  string        `json:"store_id"`
+			StoreName                string        `json:"store_name"`
+			TerminalID               string        `json:"terminal_id"`
+			Type                     string        `json:"type"`
+			ZeroInterestInstallments string        `json:"zero_interest_installments"`
+			ChargeStatus             string        `json:"charge_status"`
+			ReceiptAmount            string        `json:"receipt_amount"`
+			Discounts                []interface{} `json:"discounts"`
+		} `json:"source"`
+		Schedule                 string    `json:"schedule"`
+		Customer                 string    `json:"customer"`
+		Dispute                  string    `json:"dispute"`
+		Transaction              string    `json:"transaction"`
+		FailureCode              string    `json:"failure_code"`
+		FailureMessage           string    `json:"failure_message"`
+		Status                   string    `json:"status"`
+		AuthorizeURI             string    `json:"authorize_uri"`
+		ReturnURI                string    `json:"return_uri"`
+		CreatedAt                time.Time `json:"created_at"`
+		PaidAt                   string    `json:"paid_at"`
+		ExpiresAt                time.Time `json:"expires_at"`
+		ExpiredAt                string    `json:"expired_at"`
+		ReversedAt               string    `json:"reversed_at"`
+		ZeroInterestInstallments bool      `json:"zero_interest_installments"`
+		Branch                   string    `json:"branch"`
+		Terminal                 string    `json:"terminal"`
+		Device                   string    `json:"device"`
+		Authorized               bool      `json:"authorized"`
+		Capturable               bool      `json:"capturable"`
+		Capture                  bool      `json:"capture"`
+		Disputable               bool      `json:"disputable"`
+		Livemode                 bool      `json:"livemode"`
+		Refundable               bool      `json:"refundable"`
+		Reversed                 bool      `json:"reversed"`
+		Reversible               bool      `json:"reversible"`
+		Voided                   bool      `json:"voided"`
+		Paid                     bool      `json:"paid"`
+		Expired                  bool      `json:"expired"`
+	} `json:"data"`
+	Key       string    `json:"key"`
+	CreatedAt time.Time `json:"created_at"`
 }
