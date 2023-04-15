61aaeec686f9522ccd53b48bd84dc1f966ce9391
Poon <6330137821@student.chula.ac.th>
2023-04-15 21:31:21 +0700

    fix: remove payment information from student register

--- Frontend/src/datas/SignUp.role.js
+++ Frontend/src/datas/SignUp.role.js
@@ -1,8 +1,8 @@
 const signupContent = {
     contents: [['First Name', 'Last Name'], ['Username'], ['Email'], ['Password', 'Confirm Password'], 
-                ['Address'], ['Contact Number'], ['Gender', 'Birth Date'],['Card Holder Name'],['Card Number'],['Expiry Date','CVV']]
+                ['Address'], ['Contact Number'], ['Gender', 'Birth Date']]
 };
 export const tutorSpecificContent = {
-    contents: [['Citizen ID'],['Description']]
+    contents: [['Citizen ID'],['Description'],['Card Holder Name'],['Card Number'],['Expiry Date','CVV']]
 };
 export default signupContent;
