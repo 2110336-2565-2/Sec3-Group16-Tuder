ff39e8f00bd8d10688a1dd658afc0e1226a5155d
bxntn <nuttanan118123@gmail.com>
2023-04-26 12:47:00 +0700

    fix: use await instead of .then

--- Frontend/src/handlers/signUpHandler.js
+++ Frontend/src/handlers/signUpHandler.js
@@ -1,25 +1,15 @@
 import api from './apiHandler';
 
 export default async function signUpHandler(signUpData , navigate){
-    await api.post('/api/v1/signUp', signUpData)
-    .then(function(response){
-            
-            let res = response.data;
-            // do some response handling
-            if( res.success === true){
-                
-                navigate('/sign-in')
-            }
-            
+    try{
+        const response =  await api.post('/api/v1/signUp', signUpData)
+        navigate('/sign-in')
+        return response;
+    }catch(error){
+        if (error.response) {
+            let res = error.response.data;
+            throw Error(res.message);
+        }
+    }
 
-        }).catch(function(error){
-            // if internal error occurs, MOO will return error message
-            if (error.response) {
-                let res = error.response.data;
-                throw Error(res.message);
-            }
-        });
-    };
-
-
-
+};
