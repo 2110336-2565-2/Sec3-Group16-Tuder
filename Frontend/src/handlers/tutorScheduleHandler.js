2ac98227fcb29f476309da8e2d08ea93fcca9d26
Natchapon Chitturongaporn <88874631+moocandygumlol@users.noreply.github.com>
2023-04-26 20:54:21 +0700

    fix : middleware and put header

--- Frontend/src/handlers/tutorScheduleHandler.js
+++ Frontend/src/handlers/tutorScheduleHandler.js
@@ -2,5 +2,10 @@ import api from './apiHandler';
 import { fetchCourseByIdHandler } from './searchCourseHandler';
 
 export const fetchTutorSchedule = (courseID) => {
-    return api.get('api/v1/tutor/schedule/'+ courseID)
+    return api.get('api/v1/tutor/schedule/'+ courseID, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + localStorage.getItem("jwtToken"),
+        }
+      })
 }
