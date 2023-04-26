d92295bff1a4639743c0c13ef99477a3173073e2
Napat Srithavalya <ball04407@hotmail.com>
2023-04-27 01:56:21 +0700

    remove middleware reset password

--- Backend/internal/routes/route.resetpassword.go
+++ Backend/internal/routes/route.resetpassword.go
@@ -14,12 +14,8 @@ func InitResetPasswordRoute(c *ent.Client, e *echo.Group) {
 	repoResetPassword := repository.NewRepositoryResetPassword(c)
 	serviceResetPassword := services.NewServiceResetPassword(repoResetPassword)
 	controllerResetPassword := controller.NewControllerResetPassword(serviceResetPassword)
-	middlewareInst := middlewares.NewAuthMiddleware(os.Getenv("JWT_SECRET"))
 
 	e.POST("/forget-password", controllerResetPassword.ForgetPassword)
-
-	//e.POST("/reset-password", controllerResetPassword.ResetPassword)
 	resetPasswordRoute := e.Group("/reset-password")
-	resetPasswordRoute.Use(middlewareInst.JWT())
 	resetPasswordRoute.POST("", controllerResetPassword.ResetPassword)
 }
